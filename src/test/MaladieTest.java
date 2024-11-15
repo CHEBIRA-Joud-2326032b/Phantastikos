6ea649025b80310bb52f7a91a5aefdb39db73294
Batman <joud.chebira@etu.univ-amu.fr>
2024-11-15 17:24:32 +0100

    début test

---
+++ src/test/MaladieTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import main.maladies.Maladie;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaladieTest {
+
+    @Test
+    void augmenterNiveauTest() {
+        Maladie pec = new Maladie("Porphyrie érythropoïétique congénitale", "PEC", 2, 5);
+        pec.augmenterNiveau();
+        assertEquals(3, grippe.getNiveauActuel());
+    }
+
+    @Test
+    void diminuerNiveauTest() {
+        Maladie pec = new Maladie("Porphyrie érythropoïétique congénitale", "PEC", 2, 5);
+        pec.diminuerNiveau();
+        assertEquals(1, pec.getNiveauActuel());
+    }
+
+    @Test
+    void estLetaleTest() {
+        Maladie pec = new Maladie("Porphyrie érythropoïétique congénitale", "PEC", 5, 5);
+        assertFalse(pec.estLetale());
+    }
+}
+
