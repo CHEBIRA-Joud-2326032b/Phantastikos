ec843e00ea5cf7f19d2f944f24a2cf7e6f6dc80c
DOUCHY-Maxime-2326038u <146344643+DOUCHY-Maxime-2326038u@users.noreply.github.com>
2024-11-18 10:40:08 +0100

    premières méthodes classe medecin, serviceMedical et Creatures

--- src/main/creatures/Creature.java
+++ src/main/creatures/Creature.java
@@ -1,4 +1,14 @@
 package main.creatures;
 
 public class Creature {
+    private String nom;
+
+    public Creature(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
 }
