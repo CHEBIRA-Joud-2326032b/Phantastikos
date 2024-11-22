ff998c98494e9b354908701bbfc366bb941c1b59
CHEBIRA-Joud-2326032b <146094109+CHEBIRA-Joud-2326032b@users.noreply.github.com>
2024-11-22 09:40:17 +0100

    Delete src directory

--- src/main/creatures/categories/Vip.java
+++
@@ -1,9 +0,0 @@
-package main.creatures.categories;
-
-public interface Vip extends ActionsCreature {
-
-    @Override
-    default void attendre() {
-
-    }
-}
--- src/test/CreatureTest.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-import main.creatures.Creature;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class CreatureTest {
-}
